aba85fd2b91b13455ba00bc8d4c39736be7cb3ae
Anika Ayesha <140040473+anika2108020@users.noreply.github.com>
2023-07-23 22:06:37 +0600

    Add files via upload

---
+++ Experiment_1/p1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main ()
+{
+ printf("Name: Anika Tabassum Ayesha\n");
+ printf("ID: 2108020 \n");
+ printf("Dept: ETE\n");
+ printf("Address: CUET");
+}
+
---
+++ Experiment_1/p2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main ()
+{
+ int a;
+ float area;
+ a=20;
+ area=(3.1416*a*a);
+ printf("The area of the circle is :%.2f sq.unit",area);
+}
+
